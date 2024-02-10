0c5a988b33f7e505aaef7602991ea17eafbd28db
Klif <sang.wicklife@student.moringaschool.com>
2024-02-10 13:31:31 +0300

    Code along done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Add NavLink to import */
+import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
 
-function Home() {
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+/* define the NavBar component */
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/login">Login</NavLink>
     </div>
   );
 }
 
+function Home() {
+  return <h1>Home!</h1>;
+}
+
+function About() {
+  return <h1>This is my about component!</h1>;
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+/* add the NavBar component to our App component */
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
